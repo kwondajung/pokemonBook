791e8df73aa8b9b4e1d5e5fe09bb51046d6cf29f
dajung <kwonkkan@naver.com>
2024-08-21 23:13:59 +0900

    🔥 Remove: 필요 없는 import 삭제

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
-import PokemonCard from './PokemonCard';
 
 function Dashboard({ selcetdPokemon }) {
   return (
-    <DashboardContainer>
-      <h2>내 포켓볼</h2>
-      {selcetdPokemon.length === 0 ? (
-        <p>선택된 포켓몬이 없습니다.</p>
-      ) : (
-        <ul>
-          {selcetdPokemon.map((pokemon) => {
-            return <li key={pokemon.id}>{pokemon.korean_name}</li>;
-          })}
-        </ul>
-      )}
-    </DashboardContainer>
+    <>
+      <div>
+        <h2 style={{ textAlign: 'center' }}>내 포켓볼</h2>
+        <DashboardContainer>
+          {selcetdPokemon.length === 0 ? (
+            <p>선택된 포켓몬이 없습니다.</p>
+          ) : (
+            <>
+              {selcetdPokemon.map((pokemon) => {
+                // return <li key={pokemon.id}>{pokemon.korean_name}</li>;
+                return (
+                  <Card key={pokemon.id}>
+                    <img src={pokemon.img_url} />
+                    <p>{pokemon.korean_name}</p>
+                    <button>{pokemon.isSelected}</button>
+                  </Card>
+                );
+              })}
+            </>
+          )}
+        </DashboardContainer>
+      </div>
+    </>
   );
 }
 
 export default Dashboard;
 
 const DashboardContainer = styled.div`
-  background-color: #dbdbdb;
-  height: 300px;
+  background-color: #dbdbdb; //994848
+  width: 1200px;
   text-align: center;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  justify-items: center;
+  gap: 10px;
+  padding: 10px;
+  margin: auto;
+  border-radius: 10px;
+`;
+
+const Card = styled.div`
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  display: block;
+  cursor: pointer;
+  padding: 10px;
 `;
