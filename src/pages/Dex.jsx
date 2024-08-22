6f8272f89df48e7e7417aa5543a6e6feb3d0e83a
dajung <kwonkkan@naver.com>
2024-08-22 15:45:13 +0900

    💡 Update: 주석 수정

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -8,22 +8,14 @@ import PokemonCard from '../components/PokemonCard';
 
 function Dex() {
   const [selcetdPokemon, setSelctedPokemon] = useState([]);
+
+  // mock_data state 초기값으로 빼기 = > 포켓볼 상태, 포켓몬 카드 상태
   const [pokemon, setPokemon] = useState([]); // mock_data
 
   // 포켓몬 선택하기
   // 1. 같은 id를 가진 포켓몬 중복 선택 시 => alert
   // 2. 그렇지 않으면 추가
-  // 3. 배열의 개수가 6이 넘을 시 => alert
-
-  // 👇원래 코드 (작동 잘 됨)
-  // const addPokemon = (pokemon) => {
-  //   if (selcetdPokemon.length > 5) {
-  //     return alert('포켓몬은 최대 6마리만 데리고 갈 수 있어요!');
-  //   } else {
-  //     const newArr = [...selcetdPokemon, pokemon];
-  //     setSelctedPokemon(newArr);
-  //   }
-  // };
+  // 3. 배열의 길이가 5를 넘을 시 => alert
 
   const addPokemon = (pokemon) => {
     // 중복 찾기: id끼리 비교해서 중복된 게 있으면 true => 중복값 반환 => 중복값 findPoke에 할당
@@ -44,15 +36,6 @@ function Dex() {
     } else {
       return alert(`${pokemon.korean_name}은(는) 이미 데리고 있어요!`);
     }
-
-    // if(findPoke){
-    //   alert('중복');
-    //   return;
-    // } else {
-    //   const newArr = [...selcetdPokemon, pokemon];
-    //   setSelctedPokemon(newArr);
-    //   return;
-    // }
   };
 
   // 선택한 포켓몬 제거하기
@@ -78,5 +61,3 @@ function Dex() {
 }
 
 export default Dex;
-
-// mock_data state 초기값으로 빼기 = > 포켓볼 상태, 포켓몬 카드 상태
