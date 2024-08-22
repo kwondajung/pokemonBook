4bfa445304caa9a46878020892c864d13c18c062
dajung <kwonkkan@naver.com>
2024-08-22 23:18:23 +0900

    📝 Update: 포켓몬 디테일 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dex from './pages/Dex';
 import Home from './pages/Home';
+import PokemonDetail from './pages/PokemonDetail';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dex" element={<Dex />} />
-          {/* 디테일 페이지 추가하기 */}
+          <Route path="/pokemon-detail" element={<PokemonDetail />} />
         </Routes>
       </BrowserRouter>
     </>
