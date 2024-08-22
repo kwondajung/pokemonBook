af03ca736963735214a4baa2294163f90b09d2c6
dajung <kwonkkan@naver.com>
2024-08-22 23:15:32 +0900

    📝 Update: 베이스 코드 작성

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
+import MOCK_DATA from '../mock';
 
 const PokemonDetail = () => {
-  //   const params = useParams();
-  //   const id = params.id;
-
+  // const params = useParams();
+  // const id = params.id;
   const [searchParams, setSearchParams] = useSearchParams();
-
   const id = searchParams.get('id');
-  //   const name = searchParams.get('name');
-  console.log(id);
+  // console.log(id);
+
+  const pokemonList = { MOCK_DATA };
+  const newPokemon = pokemonList.find(function (findPoke) {
+    if (findPoke.id === pokemonList.id) {
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   return <DetailInfo>{id}번 포켓몬</DetailInfo>;
 };
