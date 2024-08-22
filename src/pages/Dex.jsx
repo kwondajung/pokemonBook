140d261d5b71028938c166eb30c1cebd1e172ad3
dajung <kwonkkan@naver.com>
2024-08-22 13:07:53 +0900

    ✨ New: 선택 가능한 포켓몬 6마리로 제한

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -8,11 +8,24 @@ function Dex() {
   const [selcetdPokemon, setSelctedPokemon] = useState([]);
 
   // 포켓몬 선택하기
+  // 1. 같은 id를 가진 포켓몬 중복 선택 시 => alert
+  // 2. 그렇지 않으면 추가
+  // 3. 배열의 개수가 6이 넘을 시 => alert
+  // const addPokemon = (pokemon) => {
+  //   const newArr = [...selcetdPokemon, pokemon];
+  //   setSelctedPokemon(newArr);
+  // };
+  // console.log(selcetdPokemon);
+
   const addPokemon = (pokemon) => {
-    const newArr = [...selcetdPokemon, pokemon];
-    setSelctedPokemon(newArr);
+    if (selcetdPokemon.length > 5) {
+      return alert(`포켓몬은 최대 6마리만 데리고 갈 수 있어요!`);
+    } else {
+      const newArr = [...selcetdPokemon, pokemon];
+      setSelctedPokemon(newArr);
+    }
   };
-  console.log(selcetdPokemon);
+
   // 선택한 포켓몬 제거하기
   const removePokemon = (pokemon) => {
     const delPoke = selcetdPokemon.filter(function (del) {
